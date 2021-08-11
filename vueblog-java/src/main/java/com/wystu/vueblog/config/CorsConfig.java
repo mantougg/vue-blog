bcd62b0568d64c14f0ef17c2643a895392e34ad2
wy <mantouhh@163.com>
2021-08-11 19:47:05 +0800

    跨域处理

---
+++ vueblog-java/src/main/java/com/wystu/vueblog/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.wystu.vueblog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author WY
+ * @description: CorsConfig解决跨域问题
+ * @date 2021/8/11 19:40
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
